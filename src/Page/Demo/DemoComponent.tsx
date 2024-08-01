d7a8b177b13449a357be1782e09e2f6c5c0897a7
Amar Nath <aamarnathkp@gmail.com>
2024-08-01 20:11:04 +0530

    fix: cleanup demo component

--- src/Page/Demo/DemoComponent.tsx
+++ src/Page/Demo/DemoComponent.tsx
@@ -1,38 +1,12 @@
 import React, { useState } from "react";
 // import AKPButton from "../../components/Button/AKPButton";
-import AKPModal from "../../components/Modal/AKPModal";
-import {
-    DemoContainer,
-    // DemoPropSection,
-    // DemoComponentSection,
-} from "./AKPDemo";
-import AKPButton from "../../components/Button/AKPButton";
+// import AKPModal from "../../components/Modal/AKPModal";
+import { DemoContainer } from "./AKPDemo";
 
 type Props = {};
 
 const DemoComponent = (props: Props) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const onCloseHandler = () => setIsModalOpen(false);
-
-    return (
-        <DemoContainer>
-            <AKPButton
-                type='primary'
-                size='large'
-                onClick={() => {
-                    setIsModalOpen(true);
-                }}>
-                Open Modal
-            </AKPButton>
-            <AKPModal
-                size='medium'
-                currentTheme='dark'
-                open={isModalOpen}
-                onClose={onCloseHandler}
-            />
-        </DemoContainer>
-    );
+    return <DemoContainer>{/* test new components here! */}</DemoContainer>;
 };
 
 export default DemoComponent;
